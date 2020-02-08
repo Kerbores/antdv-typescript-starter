be60416c9342680b43c44690ead0be511c1b4f14
kerbores <wangguiyuan@chinarecrm.com.cn>
2020-02-08 22:17:55 +0800

    :wrench: lint 规则配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-console": "off",
+    "no-debugger": "off"
   }
 };
