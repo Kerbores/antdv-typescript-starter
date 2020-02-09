463cbb40e46bd477a195b00837846be584e6a416
kerbores <wangguiyuan@chinarecrm.com.cn>
2020-02-09 16:01:58 +0800

    :construction: 安装鉴权指令

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,4 +3,7 @@ import Storage from "vue-ls";
 import config from "@/core/config";
 import "./ant-design-vue";
 import "@/core/filters";
+import Auth from "@/core/directives/auth";
+
 Vue.use(Storage, config.storageOptions);
+Vue.use(Auth);
